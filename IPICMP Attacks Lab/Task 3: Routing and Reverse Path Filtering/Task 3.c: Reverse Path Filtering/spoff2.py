4d36c586594d260aba983f23c623fe1e611a72d8
SaifSecure <155774359+herzSaifElarbi@users.noreply.github.com>
2025-02-04 21:13:53 +0100

    Delete IPICMP Attacks Lab directory

--- IPICMP Attacks Lab/Task 3: Routing and Reverse Path Filtering/Task 3.c: Reverse Path Filtering/spoff2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from scapy.all import *
-a = IP()
-a.src = "192.168.60.100" #192.168.60.0/24
-a.dst = "192.168.60.5"
-b = ICMP()
-pkt = a/b
-send(pkt)
--- IPICMP Attacks Lab/Task 3: Routing and Reverse Path Filtering/Task 3.c: Reverse Path Filtering/spoff3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from scapy.all import *
-a = IP()
-a.src = "1.2.3.4" #1.2.3.4.
-a.dst = "192.168.60.5"
-b = ICMP()
-pkt = a/b
-send(pkt)
